cc0c35a6ee5b2273a18d769e699b2ac53c281961
hardcyder <hardcyder.thepony@gmail.com>
2016-06-24 16:27:56 -0600

    initial commit

---
+++ Assets/Scripts/AudioVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioVisualizer : MonoBehaviour {
+		
+	public Transform[] audioSpectrumObjects;
+	public float heightMultiplier;
+	public int numberOfSamples = 1024;
+	public FFTWindow fftWindow;
+	public float lerpTime = 1;
+	
+	void Update() {
+
+		// initialize our float array
+		float[] spectrum = new float[numberOfSamples];
+
+		// populate array with fequency spectrum data
+		GetComponent<AudioSource>().GetSpectrumData(spectrum, 0, fftWindow);
+
+
+		// loop over audioSpectrumObjects and modify according to fequency spectrum data
+		// this loop matches the Array element to an object on a One-to-One basis.
+		for(int i = 0; i < audioSpectrumObjects.Length; i++)
+		{
+
+			// apply height multiplier to intensity
+			float intensity = spectrum[i] * heightMultiplier;
+
+			// calculate object's scale
+			float lerpY = Mathf.Lerp(audioSpectrumObjects[i].localScale.y,intensity,lerpTime);
+			Vector3 newScale = new Vector3( audioSpectrumObjects[i].localScale.x, lerpY, audioSpectrumObjects[i].localScale.z);
+
+			// appply new scale to object
+			audioSpectrumObjects[i].localScale = newScale;
+
+		}
+	}
+
+}
